bcd36821a9f4984b607fb0806accd9442194dbc8
Nolan Seokane <nolanseokane@Nolans-MacBook-Pro.local>
2021-02-24 09:11:22 +0200

    Added an alert (Not functional)

--- TODO/frontend/src/app/todo/todo.component.ts
+++ TODO/frontend/src/app/todo/todo.component.ts
@@ -13,9 +13,9 @@ export class TodoComponent implements OnInit {
 	constructor(private _todo: TodoService) { }
 
 	// Initialization
+	task: any = {};
 	tasks: any = [];
-	task: any = {}
-	update_task: any = false
+	update_task: any = false;
 
 	// Task Form
 	taskForm: any = new FormGroup({
@@ -24,59 +24,59 @@ export class TodoComponent implements OnInit {
 	});
 
 	ngOnInit(): void {
-		this.load()
-	}
+		this.load();
+	};
 
 	// Loading all the tasks
 	load(){
 		this._todo.getTasks().subscribe((tasks: any) => {
-			this.tasks = tasks
-			this.taskForm.reset()
-			this.update_task = false
-		})
-	}
+			this.tasks = tasks;
+			this.taskForm.reset();
+			this.update_task = false;
+		});
+	};
 
 	// Adding a new task
 	submit(taskForm: any){
 		this.taskForm.controls.complete.setValue(false);
 		this._todo.addTask(taskForm.value).subscribe((res: any) => {
-			this.load()
-		})
-	}
+			this.load();
+		});
+	};
 
 	// Mark task as complete or incomplete
 	complete(task: any){
 		this._todo.getTask(task).subscribe((task_get: any) => {
-			task_get.complete = !task_get.complete
+			task_get.complete = !task_get.complete;
 			this._todo.updateTask(task_get).subscribe((task_update: any) => {
-				this.load()
-			})
-		})
-	}
+				this.load();
+			});
+		});
+	};
 
 	// Get task to update and asign the response to this.task
 	get_task(task: any){
 		this._todo.getTask(task).subscribe((res: any) => {
-			this.task = res
+			this.task = res;
 			this.taskForm.controls.task.setValue(res.task);
-			this.update_task = true
-		})
-	}
+			this.update_task = true;
+		});
+	};
 
 	// Update the task with data from the form
 	update(taskForm: any){
-		this.task.task = taskForm.value.task
+		this.task.task = taskForm.value.task;
 		this._todo.updateTask(this.task).subscribe((res: any) => {
-			this.load()
-		})
-	}
+			this.load();
+		});
+	};
 
 	// Remove task by id
 	remove(task: any){
 		this._todo.getTask(task).subscribe((task_get: any) => {
 			this._todo.removeTask(task).subscribe((res: any) => {
-				this.load()
-			})
-		})
-	}
+				this.load();
+			});
+		});
+	};
 }
